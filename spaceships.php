25b333176fe51be39aec0be282819218edd9fc80
David Gimenez <93078743+David-Gimenez-Hernandez@users.noreply.github.com>
2023-11-04 14:31:11 +0100

    Create spaceships.php

---
+++ spaceships.php
@@ -0,0 +1,9 @@
+[{ "name": "Earth", "distance": 150, "gravity": 10, "diameter": 12750, "image": "planet-earth.png" },
+{ "name": "Jupiter", "distance": 778, "gravity": 26, "diameter": 143000, "image": "planet-jupiter.png" },
+{ "name": "Mars", "distance": 228, "gravity": 4, "diameter": 6800, "image": "planet-mars.png" },
+{ "name": "Pluto", "distance": 5900, "gravity": 1, "diameter": 2320, "image": "planet-pluto.png" },
+{ "name": "Venus", "distance": 108, "gravity": 9, "diameter": 12750, "image": "planet-venus.png" },
+{ "name": "Saturn", "distance": 1429, "gravity": 11, "diameter": 120000, "image": "planet-saturn.png" },
+{ "name": "Mercury", "distance": 58, "gravity": 4, "diameter": 4900, "image": "planet-mercury.png" },
+{ "name": "Neptune", "distance": 4500, "gravity": 12, "diameter": 50500, "image": "planet-neptune.png" },
+{ "name": "Uranus", "distance": 2870, "gravity": 9, "diameter": 52400, "image": "planet-uranus.png" } ]
